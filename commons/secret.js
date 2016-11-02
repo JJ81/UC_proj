d4a84e5c8bd7743a39a3a9768f2e4b43949971dc
yijaejun <yijaejun81@gmail.com>
2016-11-02 22:31:17 +0900

    set mysql

---
+++ commons/secret.js
@@ -0,0 +1,29 @@
+/**
+ * Created by yijaejun on 2016. 11. 2..
+ */
+module.exports = (function (){
+	return {
+		scretkey: 'thisismysecretforucproj',
+		'local': {
+			host: '127.0.0.1',
+			port: '3306',
+			user: 'root',
+			password: '',
+			database: 'uc_proj'
+		},
+		'dev' : {
+			host: '127.0.0.1',
+			port: '3306',
+			user: 'root',
+			password: '',
+			database: 'uc_proj'
+		},
+		'real' : {
+			host: '127.0.0.1',
+			port: '3306',
+			user: 'root',
+			password: '',
+			database: 'uc_proj'
+		}
+	}
+})();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var mysql_dbc = require('../commons/db_conn')();
+var connection = mysql_dbc.init();
+mysql_dbc.test_open(connection);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'UC_proj' });
 });
 
 module.exports = router;
