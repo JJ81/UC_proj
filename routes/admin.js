4a6e3c305774e509d945ea8c737e0bca621861f2
yijaejun <yijaejun81@gmail.com>
2016-11-08 23:50:15 +0900

    no message

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/login', function(req, res, next) {
+	res.render('admin/login', {
+		title: 'UC_proj admin page, login'
+	});
+});
 
-router.get('/', function(req, res, next) {
+router.get('/dashboard', function(req, res, next) {
 	res.render('admin/index', {
 		title: 'UC_proj admin page'
 	});
 });
+
 
 module.exports = router;
